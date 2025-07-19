a90e7d0d3a3f36d8d7638282b1930a75596dc702
Prajyot Bhatkar <prajyotbhatkar11@gmail.com>
2025-07-19 16:22:46 +0530

    new project

---
+++ accSolu-login-service/src/test/java/com/account/solution/accSolu_login_service/AccSoluLoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.account.solution.accSolu_login_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccSoluLoginServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
