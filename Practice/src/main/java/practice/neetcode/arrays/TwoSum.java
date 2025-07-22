f2dee1380ab4185a1e270806ab351d19d552d0d9
Prajyot Bhatkar <prajyotbhatkar11@gmail.com>
2025-07-23 00:52:54 +0530

    neet code 2 done 3rd started

---
+++ Practice/src/main/java/practice/neetcode/arrays/TwoSum.java
@@ -0,0 +1,15 @@
+package practice.neetcode.arrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] A = new int[]{3,4,5,6};
+//        System.out.println(twoSum(A));
+    }
+
+//    public static int [] twoSum(int[] nums) {
+//        return
+//    }
+}
--- Practice/src/main/java/practice/neetcode/arrays/ValidAnagram.java
+++ Practice/src/main/java/practice/neetcode/arrays/ValidAnagram.java
@@ -1,21 +1,58 @@
 package practice.neetcode.arrays;
 
 import java.util.HashMap;
+import java.util.Optional;
 
 public class ValidAnagram {
     public static void main(String[] args) {
-        String s ="racecar";
-        String t = "carrace";
+        String s ="bbcc";
+        String t = "ccbc";
         System.out.println(isAnagram(s, t));
+        System.out.println(isAnagramEasy(s, t));
     }
 
     public static boolean isAnagram(String s, String t) {
         HashMap<Character,Integer> charCount = new HashMap<>();
+        if (s.length() != t.length()){
+            return false;
+        }
         for (int i =0 ; i< s.length(); i++){
             if (charCount.containsKey(s.charAt(i))){
-
+                charCount.put(s.charAt(i),charCount.get(s.charAt(i))+1);
+            }else {
+                charCount.put(s.charAt(i),1);
             }
         }
-        return false;
+
+        for (int i =0; i<t.length(); i++){
+            if (charCount.containsKey(t.charAt(i)) && charCount.get(t.charAt(i))> -1){
+                charCount.put(t.charAt(i),charCount.get(t.charAt(i))-1);
+            }
+            else {
+                return false;
+            }
+        }
+        long count = charCount.values()
+                .stream()
+                .filter(x -> x != 0)
+                .count();
+        if (count > 0){
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isAnagramEasy(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> countS = new HashMap<>();
+        HashMap<Character, Integer> countT = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            countS.put(s.charAt(i), countS.getOrDefault(s.charAt(i), 0) + 1);
+            countT.put(t.charAt(i), countT.getOrDefault(t.charAt(i), 0) + 1);
+        }
+        return countS.equals(countT);
     }
 }
